44dd01f810266064c193c2daa28ade0e06db2178
ywci <root@wing-virtual-machine.(none)>
2015-01-26 05:21:59 +0800

    Update tunnel

--- lib/tunnel.py
+++ lib/tunnel.py
@@ -23,6 +23,7 @@ import crypto
 import socket
 import psutil
 from log import log
+from lock import VDevLock
 from subprocess import Popen
 from util import DEFAULT_UID, DEFAULT_TOKEN, ifaddr, send_pkt, recv_pkt
 from conf.virtdev import VDEV_SUPERNODE_PORT, VDEV_SUPERNODES, VDEV_FS_PORT
@@ -35,74 +36,175 @@ RETRY_MAX = 50
 SLEEP_TIME = 0.1 # seconds
 TIMEOUT = 10
 
-def _split(addr):
-    return addr.split(':')
-
-def _iface(addr): 
-    name = ''
-    fields = _split(addr)[1].split('.')
-    for i in range(1, 4):
-        name += '%02x' % int(fields[i])
-    return name
-
-def _tunnel(addr):
-    grp, addr = _split(addr)
-    if int(grp) > 255:
-        raise Exception('invalid address')
-    name = '%02x' % int(grp)
-    fields = addr.split('.')
-    for i in range(1, 4):
-        name += '%02x' % int(fields[i])
-    return name
-
-def _chkiface(addr):
-    address = None
-    for _ in range(RETRY_MAX):
+def excl(func):
+    def _excl(*args, **kwargs):
+        self = args[0]
+        addr = args[1]
+        lock = self._lock.acquire(addr)
         try:
-            ifname = _iface(addr)
-            address = ifaddr(ifname)
-            if address:
-                return address
-            time.sleep(SLEEP_TIME)
-        except:
-            time.sleep(SLEEP_TIME)
-    log('tunnel: failed to check iface')      
-    raise Exception('tunnel: failed to check iface')
+            return func(*args, **kwargs)
+        finally:
+            lock.release()
+    return _excl
 
-def _check_pid(pid):
-    for _ in range(RETRY_MAX):
-        try:
-            if psutil.Process(pid).status() == psutil.STATUS_ZOMBIE:
-                return False
+
+class Tunnel(object):
+    def __init__(self):
+        self._tunnels = {}
+        self._lock = VDevLock()
+    
+    def _split(self, addr):
+        return addr.split(':')
+    
+    def _run_path(self, addr):
+        return '/var/run/tunnel-%s.pid' % self._iface(addr)
+    
+    def _get_reserved_address(self, addr):
+        ip = self.addr2ip(addr)
+        fields = ip.split('.')
+        n = int(fields[3])
+        return '%s.%s.%s.%d' % (fields[0], fields[1], fields[2], n + NETSIZE - 1)
+
+    def _iface(self, addr): 
+        name = ''
+        fields = self._split(addr)[1].split('.')
+        for i in range(1, 4):
+            name += '%02x' % int(fields[i])
+        return name
+    
+    def _tunnel(self, addr):
+        grp, addr = self._split(addr)
+        if int(grp) > 255:
+            raise Exception('invalid address')
+        name = '%02x' % int(grp)
+        fields = addr.split('.')
+        for i in range(1, 4):
+            name += '%02x' % int(fields[i])
+        return name
+    
+    def _supernode(self, addr):
+        code = 0
+        odd = 1
+        length = len(VDEV_SUPERNODES)
+        if length >= (1 << 16):
+            log('tunnel: too much supernodes')
+            raise Exception('tunnel: too much supernodes')
+        for i in addr:
+            if odd:
+                code ^= ord(i)
+                odd = 0
             else:
-                return True
-        except:
-            time.sleep(SLEEP_TIME)
-    return False
+                code ^= ord(i) << 8
+                odd = 1
+        return '%s:%d' % (VDEV_SUPERNODES[code % length], VDEV_SUPERNODE_PORT)
 
-def _run_path(addr):
-    return '/var/run/tunnel-%s.pid' % _iface(addr)
-
-def _supernode(addr):
-    code = 0
-    odd = 1
-    length = len(VDEV_SUPERNODES)
-    if length >= (1 << 16):
-        log('tunnel: too much supernodes')
-        raise Exception('tunnel: too much supernodes')
-    for i in addr:
-        if odd:
-            code ^= ord(i)
-            odd = 0
+    def _is_gateway(self, addr):
+        address = ifaddr(self._iface(addr))
+        fields = address.split('.')
+        return int(fields[3]) == 1
+    
+    def _check_pid(self, pid):
+        for _ in range(RETRY_MAX):
+            try:
+                if psutil.Process(pid).status() == psutil.STATUS_ZOMBIE:
+                    return False
+                else:
+                    return True
+            except:
+                time.sleep(SLEEP_TIME)
+        return False
+    
+    def addr2ip(self, addr):
+        return self._split(addr)[1]
+    
+    def _connect(self, addr, key, static):
+        if not static:
+            address = '0.0.0.0'
         else:
-            code ^= ord(i) << 8
-            odd = 1
-    return '%s:%d' % (VDEV_SUPERNODES[code % length], VDEV_SUPERNODE_PORT)
+            address = self._get_reserved_address(addr)
+        pid = Popen(['edge', '-r', '-d', self._iface(addr), '-a', address, '-s', NETMASK, '-c', self._tunnel(addr), '-k', key, '-l', self._supernode(addr)], stdout=DEVNULL, stderr=DEVNULL).pid
+        if not self._check_pid(pid):
+            log('tunnel: failed to connect')
+            raise Exception('tunnel: failed to connect')
+        with open(self._run_path(addr), 'w') as f:
+            f.write(str(pid))
+        if not static:
+            os.system('dhclient -q %s' % self._iface(addr))
+        return self._chkiface(addr)
+    
+    @excl
+    def connect(self, addr, key, static):
+        if self._tunnels.has_key(addr):
+            self._tunnels[addr] += 1
+        else:
+            self._connect(addr, key, static)
+            self._tunnels[addr] = 1
+    
+    def _disconnect(self, addr, force):
+        if not force:
+            if self._is_gateway(addr):
+                return
+        path = self._run_path(addr)
+        with open(path, 'r') as f:
+            pid = int(f.readlines()[0].strip())
+        os.system('kill -9 %d 2>/dev/null' % pid)
+        os.remove(path)
+    
+    @excl
+    def disconnect(self, addr, force):
+        cnt = self._tunnels.get(addr)
+        if cnt and cnt > 0:
+            self._tunnels[addr] -= 1
+            if self._tunnels[addr] == 0:
+                del self._tunnels[addr]
+                self._disconnect(addr, force)
+    
+    @excl
+    def exists(self, addr):
+        return os.path.exists(self._run_path(addr))
+    
+    def _chkiface(self, addr):
+        address = None
+        for _ in range(RETRY_MAX):
+            try:
+                ifname = self._iface(addr)
+                address = ifaddr(ifname)
+                if address:
+                    return address
+                time.sleep(SLEEP_TIME)
+            except:
+                time.sleep(SLEEP_TIME)
+        log('tunnel: failed to check iface')      
+        raise Exception('tunnel: failed to check iface')
 
-def _is_gateway(addr):
-    address = ifaddr(_iface(addr))
-    fields = address.split('.')
-    return int(fields[3]) == 1
+    @excl
+    def create(self, addr, key):
+        cfg = []
+        address = self._split(addr)[1]
+        fields = address.split('.')
+        n = int(fields[3])
+        subnet = '%s.%s.%s.%d' % (fields[0], fields[1], fields[2], n - 1)
+        start = '%s.%s.%s.%d' % (fields[0], fields[1], fields[2], n + 1)
+        end = '%s.%s.%s.%d' % (fields[0], fields[1], fields[2], n + NETSIZE - 2)
+        broadcast = '%s.%s.%s.%d' % (fields[0], fields[1], fields[2], n + NETSIZE)
+        cfg.append('ddns-update-style none;\n')
+        cfg.append('default-lease-time 14400;\n')
+        cfg.append('max-lease-time 36000;\n')
+        cfg.append('subnet %s netmask %s {\n' % (subnet, NETMASK))
+        cfg.append('  range %s %s;\n' % (start, end))
+        cfg.append('  option subnet-mask %s;\n' % NETMASK) 
+        cfg.append('  option broadcast-address %s;\n' % broadcast)
+        cfg.append('}\n')
+        with open(PATH, 'w') as f:
+            f.writelines(cfg)
+        pid = Popen(['edge', '-r', '-d', self._iface(addr), '-a', address, '-s', NETMASK, '-c', self._tunnel(addr), '-k', key, '-l', self._supernode(addr)], stdout=DEVNULL, stderr=DEVNULL).pid
+        with open(self._run_path(addr), 'w') as f:
+            f.write(str(pid))
+        os.system('killall -q -9 dhcpd')
+        os.system('dhcpd -q')
+        return self._chkiface(addr)
+
+tunnel = Tunnel()
 
 def send(sock, buf):
     send_pkt(sock, buf)
@@ -111,70 +213,22 @@ def recv(sock):
     return recv_pkt(sock)
 
 def addr2ip(addr):
-    return _split(addr)[1]
-
-def get_reserved_address(addr):
-    ip = addr2ip(addr)
-    fields = ip.split('.')
-    n = int(fields[3])
-    return '%s.%s.%s.%d' % (fields[0], fields[1], fields[2], n + NETSIZE - 1)
+    return tunnel.addr2ip(addr)
 
 def create(addr, key):
-    cfg = []
-    address = _split(addr)[1]
-    fields = address.split('.')
-    n = int(fields[3])
-    subnet = '%s.%s.%s.%d' % (fields[0], fields[1], fields[2], n - 1)
-    start = '%s.%s.%s.%d' % (fields[0], fields[1], fields[2], n + 1)
-    end = '%s.%s.%s.%d' % (fields[0], fields[1], fields[2], n + NETSIZE - 2)
-    broadcast = '%s.%s.%s.%d' % (fields[0], fields[1], fields[2], n + NETSIZE)
-    cfg.append('ddns-update-style none;\n')
-    cfg.append('default-lease-time 14400;\n')
-    cfg.append('max-lease-time 36000;\n')
-    cfg.append('subnet %s netmask %s {\n' % (subnet, NETMASK))
-    cfg.append('  range %s %s;\n' % (start, end))
-    cfg.append('  option subnet-mask %s;\n' % NETMASK) 
-    cfg.append('  option broadcast-address %s;\n' % broadcast)
-    cfg.append('}\n')
-    with open(PATH, 'w') as f:
-        f.writelines(cfg)
-    pid = Popen(['edge', '-r', '-d', _iface(addr), '-a', address, '-s', NETMASK, '-c', _tunnel(addr), '-k', key, '-l', _supernode(addr)], stdout=DEVNULL, stderr=DEVNULL).pid
-    with open(_run_path(addr), 'w') as f:
-        f.write(str(pid))
-    os.system('killall -q -9 dhcpd')
-    os.system('dhcpd -q')
-    return _chkiface(addr)
+    tunnel.create(addr, key)
 
 def connect(addr, key, static=False):
-    if not static:
-        address = '0.0.0.0'
-    else:
-        address = get_reserved_address(addr)
-    pid = Popen(['edge', '-r', '-d', _iface(addr), '-a', address, '-s', NETMASK, '-c', _tunnel(addr), '-k', key, '-l', _supernode(addr)], stdout=DEVNULL, stderr=DEVNULL).pid
-    if not _check_pid(pid):
-        log('tunnel: failed to connect')
-        raise Exception('tunnel: failed to connect')
-    with open(_run_path(addr), 'w') as f:
-        f.write(str(pid))
-    if not static:
-        os.system('dhclient -q %s' % _iface(addr))
-    return _chkiface(addr)
+    tunnel.connect(addr, key, static)
 
 def release(addr, force=False):
-    if not force:
-        if _is_gateway(addr):
-            return
-    path = _run_path(addr)
-    with open(path, 'r') as f:
-        pid = int(f.readlines()[0].strip())
-    os.system('kill -9 %d 2>/dev/null' % pid)
-    os.remove(path)
+    tunnel.disconnect(addr, force)
 
 def disconnect(addr, force=False):
-    release(addr, force)
+    tunnel.disconnect(addr, force)
 
 def exist(addr):
-    return os.path.exists(_run_path(addr))
+    tunnel.exists(addr)
 
 def put(addr, op, args, uid=DEFAULT_UID, token=DEFAULT_TOKEN):
     sock = socket.create_connection((addr, VDEV_FS_PORT), timeout=TIMEOUT)
