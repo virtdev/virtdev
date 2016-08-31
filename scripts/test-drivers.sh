47699d5d9de7049cca3c0f8ac8e457eb5cf202d2
virtdev <user@users-MacBook-Air.local>
2016-08-31 18:03:52 +0800

    Testing

--- scripts/test-drivers.sh
+++ scripts/test-drivers.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 CWD=`readlink -f $0`
 DIR=`dirname $CWD`
 HOME=`dirname $DIR`
@@ -6,7 +6,7 @@ BIN="$HOME/bin"
 CMD_CREATE="$BIN/create"
 USERID="00000000000000000000000000000000"
 
-echo "creating devices..."
+echo "Creating devices..."
 DRIVERS=('Blob')
 for i in ${DRIVERS[*]}; do
   `$CMD_CREATE -s -t $i -u $USERID`
